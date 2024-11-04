bd37dc8f473c068af6a94255c89b149bba4821da
valchlol <a20220134@juan23.edu.pe>
2024-11-04 13:19:10 -0500

    Add files via upload

---
+++ operaciones.js
@@ -0,0 +1,24 @@
+document.write("<h1>Operaciones Básicas</h1>");
+
+var num1 = parseInt(prompt("Ingresa primer número"));
+var num2 = parseInt(prompt("Ingresa segundo número"));
+
+var sum = num1 + num2;
+var res = num1 - num2;
+var mu1 = num1 * num2;
+var div = num1 / num2;
+
+document.write("El primer número fue: " + num1);
+document.write("<br>");
+document.write("El segundo número fue: " + num2);
+document.write("<br>");
+document.write("La suma de ambos es:" + sum);
+document.write("<br>");
+document.write("La resta de ambos es:" + res);
+document.write("<br>");
+document.write("La producto de ambos es:" + mu1);
+document.write("<br>");
+document.write("La división de ambos es:" + div);
+document.write("<br>");
+document.write("<br>");
+document.write("<maquee><h2>Hecho por Valentina Portocarrero</h2></marquee>");
